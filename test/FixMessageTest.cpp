fe3e77a30b9200bef04c06ee47d5a38ed6827112
Andrew Kaminer <akaminer@gatech.edu>
2025-06-22 01:07:48 -0400

    oops forgot test

--- test/FixMessageTest.cpp
+++ test/FixMessageTest.cpp
@@ -1,13 +1,14 @@
 #include <gtest/gtest.h>
+
 #include "fix/FixMessage.h"
 
 TEST(FixMessageTest, FieldAddGet) {
-    FixMessage msg;
-    msg.addField(35, "D");
-    msg.addField(49, "CLIENT");
+  FixMessage msg;
+  msg.addField(35, "D");
+  msg.addField(49, "CLIENT");
 
-    EXPECT_TRUE(msg.has(35));
-    EXPECT_EQ(msg.get(35).value(), "D");
-    EXPECT_EQ(msg.get(49).value(), "CLIENT");
-    EXPECT_FALSE(msg.has(56));
+  EXPECT_TRUE(msg.has(35));
+  EXPECT_EQ(msg.get(35).value(), "D");
+  EXPECT_EQ(msg.get(49).value(), "CLIENT");
+  EXPECT_FALSE(msg.has(56));
 }
